a21602059e731937e79a8539977fcf9388b1c6c4
Chun-Kit Chung <3chunkit.c@gmail.com>
2024-06-28 20:11:35 -0400

    data preprocessing

--- amazon_analysis_final.py
+++ amazon_analysis_final.py
@@ -12,3 +12,15 @@ file_path = 'ratings_Electronics.csv'
 data = pd.read_csv(file_path, header=None)
 
 print(data)
+
+"""#Data Exploration and Preprocessing"""
+
+data.columns = ['UserID', 'ProductID', 'Rating', 'UnixTime'] # adds headers to columns
+data['UserID'] = data['UserID'].astype(str) # converts column to string
+data['ProductID'] = data['ProductID'].astype(str)
+data['RatingTimestamp'] = pd.to_datetime(data['UnixTime'], unit='s', utc=True) # Converts Unix timestamps to datetime objects
+data['RatingTimestamp'] = data['RatingTimestamp'].dt.tz_convert('America/New_York') # Adjusts datetime objects to America/New_York time zone
+data['RatingTimestamp'] = data['RatingTimestamp'].dt.strftime('%Y-%m-%d %H:%M:%S') # Converts datetime objects to strings in preferred format
+data = data.drop(columns=['UnixTime'])
+
+print(data)
